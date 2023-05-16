c021ea924daac77e54ad39595ffcb3bd76edff57
Joel Smith <joelsmith2004@gmail.com>
2023-05-16 14:45:38 +0800

    first commit

---
+++ CITS2200ProjectTester.java
@@ -0,0 +1,107 @@
+import java.io.*;
+// import java.util.*;
+
+public class CITS2200ProjectTester {
+	public static void loadGraph(CITS2200Project project, String path) {
+		// The graph is in the following format:
+		// Every pair of consecutive lines represent a directed edge.
+		// The edge goes from the URL in the first line to the URL in the second line.
+		try {
+			try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+				while (reader.ready()) {
+					String from = reader.readLine();
+					String to = reader.readLine();
+					System.out.println("Adding edge from " + from + " to " + to);
+					project.addEdge(from, to);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("There was a problem:");
+			System.out.println(e.toString());
+		}
+	}
+
+	public static void main(String[] args) {
+		// Change this to be the path to the graph file.
+		String pathToGraphFile = "testData.txt";
+		// Create an instance of your implementation.
+	    Project proj = new Project();
+		// Load the graph into the project.
+		loadGraph(proj, pathToGraphFile);
+
+		// Write your own tests!
+        addEdgeTest(proj);
+        //getShortestPathTest(proj);
+        getHamiltonianPathTest(proj);
+	}
+
+
+    public static void addEdgeTest(Project project){
+        int count = 0;
+        System.out.println("\naddEdgeTest\n");
+        for (Project.Vertex vert : project.getTree()){
+            System.out.println(vert.name());
+            for (int i = 0; i < vert.getAllLinks().size(); i++){
+                Project.Edge link = vert.getlink(i);
+                System.out.println("      " + link.getVertName() + " " + link.getIndex());
+            }
+            System.out.println("\n");
+            count++;
+        }
+        if (Project.getNumofVertex() == count){
+            System.out.println("Passed");
+        } else{
+            System.out.println("Failed");
+        }
+        System.out.println("\n\n");
+    }
+
+
+    public static void getShortestPathTest(Project project){
+        System.out.println("\ngetShortestPathTest\n");
+        int test0 = project.getShortestPath("/wiki/Maximum_flow_problem", "/wiki/Maximum_flow_problem");
+        if (test0 == 0){
+            System.out.println("Passed 0: needed 0 got " + Integer.toString(test0));
+        } else{
+            System.out.println("Failed: needed 0 got " + Integer.toString(test0));
+        }
+        int test1 = project.getShortestPath("/wiki/Maximum_flow_problem", "/wiki/Multi-commodity_flow_problem");
+        if (test1 == 2){
+            System.out.println("Passed 1: needed 2 got " + Integer.toString(test1));
+        } else{
+            System.out.println("Failed: needed 2 got " + Integer.toString(test1));
+        }
+        int test2 = project.getShortestPath("/wiki/Flow_network", "/wiki/Nowhere-zero_flow");
+        if (test2 == -1){
+            System.out.println("Passed 2: needed -1 got " + Integer.toString(test2));
+        } else{
+            System.out.println("Failed: needed -1 got " + Integer.toString(test2));
+        }
+        int test3 = project.getShortestPath("/wiki/Nowhere-zero_flow", "/wiki/Flow_network");
+        if (test3 == 1){
+            System.out.println("Passed 3: needed 1 got " + Integer.toString(test3));
+        } else{
+            System.out.println("Failed: needed 1 got " + Integer.toString(test3));
+        }
+        int test4 = project.getShortestPath("/wiki/Nowhere-zero_flow", "/wiki/Gomory%E2%80%93Hu_tree");
+        if (test4 == 6){
+            System.out.println("Passed 4: needed 6 got " + Integer.toString(test4));
+        } else{
+            System.out.println("Failed: needed 6 got " + Integer.toString(test4));
+        } 
+        int test5 = project.getShortestPath("/wiki/Nowhere-zero_flow", "/wiki/Braess%27_paradox");
+        if (test5 == 2){
+            System.out.println("Passed 5: needed 2 got " + Integer.toString(test5));
+        } else{
+            System.out.println("Failed: needed 2 got " + Integer.toString(test5));
+        }
+    }
+
+    public static void getHamiltonianPathTest(Project project){
+        System.out.println("\ngetHamiltonianPathTest\n");
+        String[] result = project.getHamiltonianPath();
+        for (String s : result){
+            System.out.println(s);
+        }
+    }
+}
---
+++ Project.java
@@ -0,0 +1,377 @@
+import java.util.ArrayList;
+
+
+
+public class Project implements CITS2200Project{
+
+    ArrayList<Vertex> tree = new ArrayList<>();
+    static int numOfVertex = 0;
+    ArrayList<String> HamiltonianPath = new ArrayList<>();
+
+    public ArrayList<Vertex> getTree(){
+        return tree;
+    }
+    public static int getNumofVertex(){
+        return numOfVertex;
+    }
+
+
+
+    @Override
+    public void addEdge(String urlFrom, String urlTo) {
+        boolean fromInTree = false;
+        boolean toInTree = false;
+        int toIndex = -1;
+
+        if (numOfVertex != 0){
+            for (Vertex vert : tree){
+                if (vert.name().equals(urlFrom)){
+                    fromInTree = true;
+                }
+                if (vert.name().equals(urlTo)){
+                    toInTree = true;
+                    toIndex = vert.getVertNum();
+                }
+
+            }
+        }
+
+        if (!fromInTree){
+            Vertex fromVertex = new Vertex(urlFrom, numOfVertex, new ArrayList<Edge>());
+            numOfVertex += 1;
+            tree.add(fromVertex);
+        }
+        if(!toInTree){
+            Vertex toVertex = new Vertex(urlTo, numOfVertex, new ArrayList<Edge>());
+            toIndex = numOfVertex;
+            numOfVertex += 1;
+            tree.add(toVertex);
+        }
+
+
+
+        
+
+        for (int i = 0; i < tree.size(); i++){
+            Vertex vert = tree.get(i);
+            if (vert.name().equals(urlFrom)){
+                vert.addLink(new Edge(urlTo, toIndex));
+            }
+            
+        }
+    }
+
+    /*
+     * Tree is a non weighted graph so dikstra, a-star ect wont work so breadth first search it is complexity O(E+V)
+     * https://en.wikipedia.org/wiki/Breadth-first_search
+     * 
+     * Return -1 if no path found
+     * 
+     */
+    public int getShortestPath(String urlFrom, String urlTo) {
+
+        if (tree.isEmpty()){
+            throw new IllegalStateException("Tree size = 0 need to addEdge() first");
+        }
+
+        if (urlFrom.equals(urlTo)){
+            return 0;
+        }
+
+        for (Vertex reset: tree){
+            reset.setExplored(0);
+        }
+
+        ArrayList<Vertex> queue = new ArrayList<>();
+
+        int rootIndex = -1;
+        int finalIndex = -1;
+
+        int count = 0;
+        boolean stop = false;
+
+
+        for (int i = 0; i < tree.size(); i++){
+            Vertex ver = tree.get(i);
+            if (ver.name().equals(urlFrom)){
+               
+                rootIndex = ver.getVertNum();
+            }
+            if (ver.name().equals(urlTo)){
+                
+                finalIndex = ver.getVertNum();
+            }
+        }
+
+        if (rootIndex == -1 || finalIndex == -1){
+            throw new IllegalArgumentException("urlFrom or urlTo not in tree");
+        }
+
+
+        // System.out.println("RootIndex = " + rootIndex);
+        // System.out.println("FinalIndex = " + finalIndex + "\n");
+
+
+        tree.get(rootIndex).setExplored(1);
+        queue.add(tree.get(rootIndex));
+
+        while (!queue.isEmpty() && !stop){
+            Vertex v = queue.remove(0);
+
+
+
+            // System.out.println("Current location: " + v.getVertNum());
+
+
+
+            if (v.getVertNum() == finalIndex){
+                stop = true;
+                break;
+            }
+
+      tree.get(v.getVertNum()).setExplored(1);
+
+
+
+            for (int eachEdge = 0; eachEdge < v.links.size(); eachEdge++){
+                Edge w = v.getlink(eachEdge);
+                int wInt = 0;
+                for (Vertex vert : tree){
+                    if (vert.name().equals(w.getVertName())){
+                        wInt = vert.getVertNum();
+                    }
+                }
+
+
+
+                // System.out.println("Checking if index " + wInt + "(" + w.getVertName() + ")" + " has been explored");
+
+
+                if (tree.get(wInt).getExplored() == 0){
+                    
+                    tree.get(wInt).setParent(v.getVertNum());
+                    // System.out.println("Setting index " + wInt + " parent to " + v.getVertNum());
+                    if (tree.get(wInt).getVertNum() == finalIndex){
+                        stop = true;
+                        break;
+                    }
+                    queue.add(tree.get(wInt));
+                    
+                    
+                }
+            }
+        }
+
+        Vertex path = tree.get(finalIndex);
+
+        if (path.getParent() == -1){
+            return -1;
+        }
+
+        
+        while (path.getVertNum() != rootIndex){
+            System.out.println(path.name());
+            // System.out.println(Integer.toString(path.parentNum));
+            count += 1;
+            path = tree.get(path.getParent());
+        }
+
+        System.out.println(path.name());
+        return count;
+    }
+
+    @Override
+    public String[] getCenters() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCenters'");
+    }
+
+    @Override
+    public String[][] getStronglyConnectedComponents() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStronglyConnectedComponents'");
+    }
+
+    /*
+     * Implimentation of a depth first search + backtracking to find if a hamilton path exits or not
+     * Complexity O(N!)
+     * 
+     * 
+     * https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/#:~:text=Hamiltonian%20Path%20is%20a%20path,Hamiltonian%20Paths%20in%20a%20graph.
+     */
+    public String[] getHamiltonianPath() {
+        if (tree.size() > 20){
+            throw new IllegalStateException("Tree too large for Hamiltonian path: tree.size() > 20");
+        }
+        if (tree.size() == 0){
+            throw new IllegalStateException("Tree size = 0 need to addEdge() first");
+        }
+
+        for (int i = 0; i < tree.size(); i++){
+            tree.get(i).setStack(1);
+            // System.out.println("Now checking " + tree.get(i).name());
+            if (dfs(tree.get(i), 1, tree.size())){
+                break;
+            }
+        }
+
+        String[] result = new String[HamiltonianPath.size()];
+        
+        for (int i = 0; i < HamiltonianPath.size(); i++){
+            result[i] = HamiltonianPath.get(HamiltonianPath.size() - 1 - i);
+        }
+
+        return result;
+    }
+
+    private boolean dfs(Vertex v, int inStackCount, int n){
+        if (inStackCount == n){
+            //System.out.println("Found Path adding " + v.name());
+            HamiltonianPath.add(v.name());
+            return true;
+        }
+        // System.out.println("       v.getAllLinks().size = " + Integer.toString(v.getAllLinks().size()));
+        for (int i = 0; i < v.getAllLinks().size(); i++){
+            //System.out.println("          v.getlink(i).getIndex() = " + Integer.toString(v.getlink(i).getIndex()));
+            //System.out.println("             tree.get(v.getlink(i).getIndex()).getStack() = " + Integer.toString(tree.get(v.getlink(i).getIndex()).getStack()));
+            if (tree.get(v.getlink(i).getIndex()).getStack() == 0){
+                tree.get(v.getlink(i).getIndex()).setStack(1);
+                // System.out.println("Now checking " + tree.get(v.getlink(i).getIndex()).name());
+                if (dfs(tree.get(v.getlink(i).getIndex()), inStackCount+1, n)){
+                    HamiltonianPath.add(v.name());
+                    return true;
+                }
+                tree.get(v.getlink(i).getIndex()).setStack(0);
+            }
+
+        }
+
+        return false;
+    }
+
+
+
+    public class Vertex{
+        private String name;
+        private int vertNum;
+        private ArrayList<Edge> links;
+        private int explored = 0;
+        private int parentNum = -1;
+        private int inStack = 0; // 0 = notInStack, 1 = inStack
+        
+
+        /*
+         * Constructor
+         */
+        public Vertex(String name, int vertex, ArrayList<Edge> link){
+            this.name = name;
+            vertNum = vertex;
+            links = link;
+
+        }
+
+        /*
+         * Gets vertex name;
+         */
+        public String name(){
+            return this.name;
+        }
+
+        /*
+         * Gets vertexs' vertNum
+         */
+        public int getVertNum(){
+            return vertNum;
+        }
+
+        /*
+         * Gets explored
+         */
+        public int getExplored(){
+            return explored;
+        }
+
+        /*
+         * Returns Edge i from links
+         */
+        public Edge getlink(int i){
+            return links.get(i);
+        }
+
+        public ArrayList<Edge> getAllLinks(){
+            return links;
+        }
+
+        /*
+         * Gets parent index
+         */
+        public int getParent(){
+            return parentNum;
+        }
+
+        /*
+         * gets whether vertex is in stack or not 
+         */
+        public int getStack(){
+            return inStack;
+        }
+
+        public void setStack(int i){
+            inStack = i;
+        }
+
+        /*
+         * sets parent index
+         */
+        public void setParent(int i){
+            parentNum = i;
+        }
+
+        /*
+         * Adds a link to links
+         */
+        public void addLink(Edge link){
+            links.add(link);
+        }
+
+        /*
+         * Sets explored to i
+         */
+        public void setExplored(int i){
+            explored = i;
+        }
+
+    }
+
+
+    public class Edge {
+        private String toVertex;
+        private int vertNum;
+        private int explored = 0;
+
+
+        public Edge(String to, int i){
+            toVertex = to;
+            vertNum = i;
+        }
+
+        public void setExplored(int i){
+            explored = i;
+        }
+
+        public int getIndex(){
+            return vertNum;
+        }
+
+        public String getVertName(){
+            return toVertex;
+        }
+
+        public int getExplored(){
+            return explored;
+        }
+
+
+    }
+    
+}
